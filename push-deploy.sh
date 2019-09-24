dc1075552d4536f6e15fd8e68d120e519816c56a
widyamedia <widyamedia@gmail.com>
2019-09-24 19:49:39 +0700

    rebuilding site Tue Sep 24 19:49:39 WIB 2019

--- push-deploy.sh
+++ push-deploy.sh
@@ -1,25 +1,26 @@
 #!/bin/sh
-
 # If a command fails then the deploy stops
 set -e
-
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
-
-# Build the project.
-HUGO_ENV=production hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
-
-# Go To Public folder
-cd public
-
-# Add changes to git.
+printf "\033[0;32mPush to branch hugo...\033[0m\n"
 git add .
 
-# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+ msg="$*"
+fi
+git commit -m "$msg"
+
+git push origin hugo
+
+printf "\033[0;32mDeploy public hugo to branch master...\033[0m\n"
+HUGO_ENV=production hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+cd public
+git add .
+
 msg="rebuilding site $(date)"
 if [ -n "$*" ]; then
 	msg="$*"
 fi
 git commit -m "$msg"
 
-# Push source and build repos.
 git push origin master
